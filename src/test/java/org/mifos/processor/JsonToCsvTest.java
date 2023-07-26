81c1684a6db3b6fe73bf43df239426915221d34f
truthfool <ishanranasingh@gmail.com>
2023-07-26 13:10:44 +0530

    added unit test

--- src/test/java/org/mifos/processor/JsonToCsvTest.java
+++ src/test/java/org/mifos/processor/JsonToCsvTest.java
@@ -1,23 +1,22 @@
 package org.mifos.processor;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.mifos.processor.bulk.utility.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JsonToCsvTest extends CamelTestSupport {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Produce(uri = "http://localhost:8080/batchtransaction")
     private ProducerTemplate producerTemplate;
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
     @Test
-    public void testJsonToCsv() throws Exception{
+    public void testJsonToCsv() throws Exception {
 
         String json = "[{\"payer\":{\"partyIdInfo\":{\"partyIdType\":\"MSISDN\",\"partyIdentifier\":\"27710101999\"}},\"payee\":{\"partyIdInfo\":{\"partyIdType\":\"MSISDN\",\"partyIdentifier\":\"27710102999\"}},\"amount\":{\"amount\":22.5,\"currency\":\"USD\"},\"request_id\":\"f1e22fe3-9740-4fba-97b6-78f43bfa7f2f\",\"payment_mode\":\"mojaloop\",\"note\":\"TestPayment\"},{\"payer\":{\"partyIdInfo\":{\"partyIdType\":\"MSISDN\",\"partyIdentifier\":\"344136101999\"}},\"payee\":{\"partyIdInfo\":{\"partyIdType\":\"MSISDN\",\"partyIdentifier\":\"344136101999\"}},\"amount\":{\"amount\":21.5,\"currency\":\"USD\"},\"request_id\":\"ugfgr4u23-9894-sdvv-97b6-78f43bfa7f2f\",\"payment_mode\":\"mojaloop\"}]";
 
@@ -26,17 +25,10 @@ public class JsonToCsvTest extends CamelTestSupport {
                 "1,ugfgr4u23-9894-sdvv-97b6-78f43bfa7f2f,mojaloop,MSISDN,344136101999,MSISDN,344136101999,21.5,USD,\n";
 
         // Send the JSON request
-        Exchange exchange = producerTemplate.send("http://localhost:8080/batchtransaction", e -> {
-            e.getIn().setBody(json);
-            e.getIn().setHeader("Content-Type", "application/json");
-            e.getIn().setHeader("Platform-TenantId", "gorilla");
-            e.getIn().setHeader("X-CorrelationID", "12345678");
-            e.getIn().setHeader("type", "raw");
-        });
+        String resultCsv = Utils.convertJsonToCsv(json);
 
-        String receivedCsv = exchange.getMessage().getBody(String.class);
-        logger.info("Received CSV String : {}",receivedCsv);
-        Assertions.assertEquals(expectedCsv, receivedCsv);
+        logger.info("Received CSV String : {}", resultCsv);
+        Assertions.assertEquals(expectedCsv, resultCsv);
 
     }
 }
